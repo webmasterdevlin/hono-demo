5f3c5f86e69f74882e4e61621262e32b293e7217
Devlin Duldulao <webmasterdevlin@gmail.com>
2024-04-23 21:06:22 +0200

    add tests

---
+++ tests/banana.test.ts
@@ -0,0 +1,12 @@
+import {describe, test, expect} from 'bun:test';
+
+
+describe("arithmetic", () => {
+  test("2 + 2", () => {
+    expect(2 + 2).toBe(4);
+  });
+
+  test("2 * 2", () => {
+    expect(2 * 2).toBe(4);
+  });
+});
